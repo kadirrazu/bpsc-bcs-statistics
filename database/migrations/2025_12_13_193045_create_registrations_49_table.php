ab3ef9fd6778e8be7b6635b308e23c3faa16e128
kadirrazu <razu121@gmail.com>
2026-01-14 00:28:22 +0600

    Migration added for bcs49 tables.

---
+++ database/migrations/2025_12_13_193045_create_registrations_49_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrations_49', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->integer('reg');
+            $table->string('name');
+            $table->integer('gender');
+            $table->string('dob');
+            $table->string('dob_ddmmyyyy')->nullable();
+            $table->integer('district_code');
+            $table->integer('b_subject');
+            $table->integer('g_inst_code');
+            $table->string('g_inst_name')->nullable();
+            $table->integer('graduation_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrations48');
+    }
+};
---
+++ database/migrations/2025_12_13_193057_create_final_result_49_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('final_result_49', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->integer('reg');
+            $table->string('name');
+            $table->integer('gender');
+            $table->string('dob');
+            $table->string('dob_ddmmyyyy')->nullable();
+            $table->integer('district_code');
+            $table->integer('b_subject');
+            $table->integer('g_inst_code');
+            $table->string('g_inst_name')->nullable();
+            $table->integer('graduation_year')->nullable();
+            $table->string('cadre_category')->nullable();
+            $table->string('merit_tech')->nullable();
+            $table->string('post_name')->nullable();
+            $table->string('quota_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results48');
+    }
+};
